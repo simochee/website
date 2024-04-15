6c28948f2df0d234a3438230f97bd37bc91b8d6c
simochee <lollipop@simochee.net>
2024-04-15 19:15:16 +0900

    chore(css): setup tailwindcss@next

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
+  vite: {
+    css: {
+      transformer: 'lightningcss'
+    },
+    build: {
+      cssMinify: 'lightningcss'
+    },
+    plugins: [tailwindcss()]
+  },
   image: {
     service: {
       entrypoint: 'astro/assets/services/noop'
