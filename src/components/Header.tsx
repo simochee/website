a36d85a6f98d04e3e39092ba87d8ceb4c2aece03
simochee <lollipop@simochee.net>
2024-06-06 23:43:47 +0900

    feat: menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { component$ } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
+import { Modal } from "@qwik-ui/headless";
 import logo from "~assets/logo.svg";
+import { WEBSITE_NAME } from "~constants";
 
 const NAV_ITEMS = [
 	{ href: "/", label: "プロフィール" },
@@ -13,11 +15,11 @@ export default component$(() => {
 
 	return (
 		<header class="sticky top-0 left-0 w-full bg-white bg-opacity-70 backdrop-blur-sm">
-			<div class="mx-auto flex max-w-6xl items-center justify-between p-6">
+			<div class="mx-auto flex h-16 max-w-6xl items-center justify-between pl-6 md:h-20 md:pr-6">
 				<span class="grid grid-flow-col place-items-center gap-2 text-lg">
-					<img src={logo} alt="よなよなコード" width={152} height={24} />
+					<img src={logo} alt={WEBSITE_NAME} width={152} height={24} />
 				</span>
-				<nav class="grid grid-flow-col gap-3">
+				<nav class="hidden grid-flow-col gap-3 md:grid">
 					{NAV_ITEMS.map(({ href, label }) => (
 						<a
 							key={label}
@@ -31,6 +33,35 @@ export default component$(() => {
 						</a>
 					))}
 				</nav>
+				<Modal.Root class="md:hidden">
+					<Modal.Trigger class="size-16">
+						<span class="icon-[carbon--menu] size-6" />
+					</Modal.Trigger>
+					<Modal.Panel class="mt-0 w-full">
+						<div class="flex h-16 items-center justify-between pl-6">
+							<Modal.Title>
+								<img src={logo} alt={WEBSITE_NAME} width={152} height={24} />
+							</Modal.Title>
+							<Modal.Close class="size-16">
+								<span class="icon-[carbon--close] size-6" />
+							</Modal.Close>
+						</div>
+						<nav class="grid gap-2 px-6 pb-8">
+							{NAV_ITEMS.map(({ href, label }) => (
+								<a
+									key={label}
+									href={href}
+									class={[
+										"rounded-full px-4 py-2 text-sm hover:font-bold",
+										{ "bg-wood-50 font-bold": url.pathname === href },
+									]}
+								>
+									{label}
+								</a>
+							))}
+						</nav>
+					</Modal.Panel>
+				</Modal.Root>
 			</div>
 		</header>
 	);
