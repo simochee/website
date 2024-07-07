f86bf47e6f4bdefdde81438031eb0d2e9e99b06f
simochee <lollipop@simochee.net>
2024-07-07 16:49:53 +0900

    🛀 clean up

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { qwikCity } from "@builder.io/qwik-city/vite";
-import { qwikVite } from "@builder.io/qwik/optimizer";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-	plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-});
