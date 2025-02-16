a952e57ba4f6fa982f7a6dd10807c4cdd3f2f16e
simochee <lollipop@simochee.net>
2025-02-16 23:47:55 +0900

    chore: setup tailwindcss

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet} from '@builder.io/qwik-city'
+import './global.css'
 
 export default component$(() => {
     return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
+import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+	plugins: [tailwindcss(), qwikCity(), qwikVite(), tsconfigPaths()],
 });
