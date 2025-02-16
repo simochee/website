224c9683994d0081847bcd21aaedbb7769d11722
simochee <lollipop@simochee.net>
2025-02-16 22:51:36 +0900

    feat: index page

---
+++ src/root.tsx
@@ -0,0 +1,15 @@
+import { component$ } from "@builder.io/qwik";
+import { QwikCityProvider, RouterOutlet} from '@builder.io/qwik-city'
+
+export default component$(() => {
+    return (
+        <QwikCityProvider>
+            <head>
+                <meta charset="utf-8" />
+            </head>
+            <body>
+                <RouterOutlet />
+            </body>
+        </QwikCityProvider>
+    )
+})
